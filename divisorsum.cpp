2509d13370c5e7c93f9d43f105d5c078749b8cf7
shinchan <32698751+murakamishinji@users.noreply.github.com>
2020-04-18 03:26:25 +0900

    Update divisorsum.cpp

--- divisorsum.cpp
+++ divisorsum.cpp
@@ -51,3 +51,30 @@ p[M] a[M]
 
 出力は答えを1行に
 */
+
+
+//shinchan自作3
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main() {
+    ll q,a,b;
+    cin>>q;
+    for(int i=0;i<q;i++){
+	cin>>a>>b;
+	if(a==b)cout<<0<<endl;
+	else if(a%b&&b%a)cout<<2<<endl;
+	else cout<<1<<endl; 
+    }
+    return 0;
+}
+
+
+/*
+無向グラフGに無限個の頂点1,2,3....があります。
+xがyの約数のとき頂点xと頂点yは辺で結ばれています。
+a_iとb_iからなるQ個のクエリが与えられるので、頂点a_iからb_iへのパスの長さを求めよ。
+1<=Q<=10^5
+1<=a_i<=10^18
+1<=b_i<=10^18
+*/
