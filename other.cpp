40ff5e36cb955b8e014b8cff9b4d0f462b8c681a
shinchan <32698751+murakamishinji@users.noreply.github.com>
2020-04-21 15:40:16 +0900

    ろりは

--- other.cpp
+++ other.cpp
@@ -62,6 +62,52 @@ main-> cominit();
 
 
 
+/*RollingHash
+struct RollingHash {
+    static const int base1 = 1007, base2 = 2009;
+    static const int mod1 = 1000000007, mod2 = 1000000009;
+    vector<long long> hash1, hash2, power1, power2;
+ 
+    // construct
+    RollingHash(const string &S) {
+        int n = (int)S.size();
+        hash1.assign(n+1, 0);
+        hash2.assign(n+1, 0);
+        power1.assign(n+1, 1);
+        power2.assign(n+1, 1);
+        for (int i = 0; i < n; ++i) {
+            hash1[i+1] = (hash1[i] * base1 + S[i]) % mod1;
+            hash2[i+1] = (hash2[i] * base2 + S[i]) % mod2;
+            power1[i+1] = (power1[i] * base1) % mod1;
+            power2[i+1] = (power2[i] * base2) % mod2;
+        }
+    }
+    
+    // get hash of S[left:right]
+    inline pair<long long, long long> get(int l, int r) const {
+        long long res1 = hash1[r] - hash1[l] * power1[r-l] % mod1;
+        if (res1 < 0) res1 += mod1;
+        long long res2 = hash2[r] - hash2[l] * power2[r-l] % mod2;
+        if (res2 < 0) res2 += mod2;
+        return {res1, res2};
+    }
+ 
+    // get lcp of S[a:] and T[b:]
+    inline int getLCP(int a, int b) const {
+        int len = min((int)hash1.size()-a, (int)hash1.size()-b);
+        int low = -1, high = len;
+        while (high - low > 1) {
+            int mid = (low + high) / 2;
+            if (get(a, a+mid) != get(b, b+mid)) high = mid;
+            else low = mid;
+        }
+        return low;
+    }
+};
+*/
+
+
+
 
 
 /////BIT ****(1-index)****
