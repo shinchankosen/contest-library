5bfcb2e65efdb9569d64112f62407473c22f1369
shinchan <32698751+murakamishinji@users.noreply.github.com>
2020-04-27 12:18:16 +0900

    Update other.cpp

--- other.cpp
+++ other.cpp
@@ -118,33 +118,68 @@ struct RollingHash {
 
 ///////Matrix
 /*
-k->size
-ll k,sum;
-vector<vector<ll> > mul(vector<vector<ll> > v1, vector<vector<ll> > v2){
-    vector<vector<ll> > res(k,vector<ll> (k));
-    for(int i=0;i<k;i++){
-        for(int j=0;j<k;j++){
-            sum=0;
-            for(int n=0;n<k;n++){
-                sum+=(v1[i][n]*v2[n][j]);
-            }
-            res[i][j]=sum;
-        }
+// Matrix
+template<class T> struct Matrix {
+    vector<vector<T> > val;
+    //　縦, 横, 初期値
+    Matrix(int n = 1, int m = 1, T v = 0) : val(n, vector<T>(m, v)) {}
+    void init(int n, int m, T v = 0) {val.assign(n, vector<T>(m, v));}
+    Matrix<T>& operator = (const Matrix<T> &A) {
+        val = A.val; return *this;
     }
-    return res;
+    size_t size() const {return val.size();}
+    vector<T>& operator [] (int i) {return val[i];}
+    const vector<T>& operator [] (int i) const {return val[i];}
+    friend ostream& operator << (ostream& s, const Matrix<T>& M) {
+        s << endl;
+        for (int i = 0; i < (int)M.size(); ++i) s << M[i] << endl;
+        return s;
+    }
+};
+
+template<class T> Matrix<T> operator * (const Matrix<T> &A, const Matrix<T> &B) {
+    Matrix<T> R(A.size(), B[0].size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int j = 0; j < B[0].size(); ++j)
+            for (int k = 0; k < B.size(); ++k)
+                R[i][j] += A[i][k] * B[k][j];
+    return R;
 }
 
-vector<vector<ll> > matpow(vector<vector<ll> > a,ll n) {
-    vector<vector<ll> > res(k,vector<ll> (k,0));
-    for(int i=0;i<k;i++){
-        res[i][i]=1;
+template<class T> Matrix<T> pow(const Matrix<T> &A, long long n) {
+    Matrix<T> R(A.size(), A.size());
+    auto B = A;
+    for (int i = 0; i < A.size(); ++i) R[i][i] = 1;
+    while (n > 0) {
+        if (n & 1) R = R * B;
+        B = B * B;
+        n >>= 1;
     }
-    while(n>0) {
-        if (n&1)res=mul(res,a);
-        a=mul(a,a);
-        n>>=1;
-    }
-    return res;
+    return R;
+}
+
+template<class T> vector<T> operator * (const Matrix<T> &A, const vector<T> &B) {
+    vector<T> v(A.size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int k = 0; k < B.size(); ++k)
+            v[i] += A[i][k] * B[k];
+    return v;
+}
+
+template<class T> Matrix<T> operator + (const Matrix<T> &A, const Matrix<T> &B) {
+    Matrix<T> R(A.size(), A[0].size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int j = 0; j < A[0].size(); ++j)
+            R[i][j] = A[i][j] + B[i][j];
+    return R;
+}
+
+template<class T> Matrix<T> operator - (const Matrix<T> &A, const Matrix<T> &B) {
+    Matrix<T> R(A.size(), A[0].size());
+    for (int i = 0; i < A.size(); ++i)
+        for (int j = 0; j < A[0].size(); ++j)
+            R[i][j] = A[i][j] - B[i][j];
+    return R;
 }
 */
 
