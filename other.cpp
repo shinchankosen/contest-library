cd02b473d032b9aa7d52885bffaa0dbc2ae0e2c9
shinchan <32698751+murakamishinji@users.noreply.github.com>
2020-10-06 20:32:02 +0900

    Update other.cpp

--- other.cpp
+++ other.cpp
@@ -205,3 +205,19 @@ long long extGCD(long long a, long long b, long long &x, long long &y) {
     return d;
 }
 
+
+
+
+
+//////negative vector
+struct nev{
+private:
+    ll a;
+    vector<ll> v;
+public:
+    nev(ll a, ll n = 0):a(a), v(n, 0){}
+    void resize(int m) {v.resize(m);}
+    void push_back(ll n) {v.push_back(n);}
+    size_t size() const {return v.size();}
+    ll& operator [] (int i) {return v[i - a];}
+};
