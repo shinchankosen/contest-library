c1886c1f8d958c33c7bdc9a1a3976d5522912f2f
shinchan <32698751+shinchankosen@users.noreply.github.com>
2021-10-17 23:33:53 +0900

    Update binarytree.cpp

--- binarytree.cpp
+++ binarytree.cpp
@@ -190,3 +190,63 @@ public:
     }
 };
 //update_min
+
+struct LazySegmentTree {
+private:
+    ll n;
+    vector<ll> node, lazy;
+    vector<bool> lazyFlag;
+
+public:
+    LazySegmentTree(vector<ll> v) {
+        ll sz = (int)v.size();
+        n = 1; while(n < sz) n *= 2;
+        node.resize(2*n-1);
+        lazy.resize(2*n-1, 0);
+        lazyFlag.resize(2*n-1, false);
+
+        for(int i=0; i<sz; i++) node[i+n-1] = v[i];
+        for(int i=n-2; i>=0; i--) node[i] = min(node[i*2+1], node[i*2+2]);
+    }
+
+    void lazyEvaluate(ll k, ll l, ll r) {
+        if(lazyFlag[k]) {
+            node[k] += lazy[k] / (r - l);
+            if(r - l > 1) {
+                lazy[k*2+1] += lazy[k] / 2;
+                lazy[k*2+2] += lazy[k] / 2;
+                lazyFlag[k*2+1] = lazyFlag[k*2+2] = true;
+            }
+            lazyFlag[k] = false;
+            lazy[k] = 0;
+        }
+    }
+
+    void add(int a, int b, ll x, int k=0, int l=0, int r=-1) {
+        if(r < 0) r = n;
+        lazyEvaluate(k, l, r);
+        if(b <= l || r <= a) return;
+        if(a <= l && r <= b) {
+            lazy[k] += x * (r - l);
+            lazyFlag[k] = true;
+            lazyEvaluate(k, l, r);
+        }
+        else {
+            add(a, b, x, 2*k+1, l, (l+r)/2);
+            add(a, b, x, 2*k+2, (l+r)/2, r);
+            node[k] = min(node[2*k+1], node[2*k+2]);
+        }
+    }
+
+    ll find(int a, int b, int k=0, int l=0, int r=-1) {
+        if(r < 0) r = n;
+        lazyEvaluate(k, l, r);
+        if(b <= l || r <= a) return INF;
+        if(a <= l && r <= b) return node[k];
+        ll vl = find(a, b, 2*k+1, l, (l+r)/2);
+        ll vr = find(a, b, 2*k+2, (l+r)/2, r);
+        return min(vl, vr);
+    }
+};
+
+//RMQ and RAQ
