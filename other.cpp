26db53955a43dd8de6d8fc0d531dda3846b33063
shinchan <32698751+shinchankosen@users.noreply.github.com>
2023-01-08 01:06:52 +0900

    Update other.cpp

--- other.cpp
+++ other.cpp
@@ -9,6 +9,17 @@ ll ftoll(string s, ll dig){
     return stoll(s);
 }
 
+// ll(sqrt(n)) 0 <= n <= 9 * 10^18
+ll sqrtll(ll n) { 
+    ll le = 0, ri = 3'000'000'001;
+    while(ri - le > 1) {
+        ll mid = (le + ri) / 2;
+        if(mid * mid <= n) le = mid;
+        else ri = mid;
+    }
+    return le;
+}
+
 
 ////lambda
 auto f = [&](auto&& f,int x) -> int{return x;};
