bbcf5ea55b64c8e93109e62a027b5f3b97a571e6
shinchan <32698751+shinchankosen@users.noreply.github.com>
2023-01-08 22:57:59 +0900

    Update other.cpp

--- other.cpp
+++ other.cpp
@@ -354,3 +354,16 @@ struct Fraction {
     }
     friend ostream& operator << (ostream& os, const Fraction& v) { return os << v.x << "/" << v.y; }  
 };
+
+// 転置 (vector<string>, vector<vector<T>>)
+template<class T> vector<T> Transpose(const vector<T> &v) {
+    int n = v.size(), m = v[0].size();
+    vector<T> ret(m);
+    for(int i = 0; i < m; i ++) {
+        ret[i].resize(n);
+        for(int j = 0; j < n; j ++) {
+            ret[i][j] = v[j][i];
+        }
+    }
+    return ret;
+}
