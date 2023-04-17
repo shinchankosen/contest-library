07bebaebe199597206a31b7ecc91ab93421a245a
shinchankosen <sophiaringoukosen@gmail.com>
2023-04-18 01:37:40 +0900

    comb

--- source/math_number/combination.cpp
+++ source/math_number/combination.cpp
@@ -1,20 +1,19 @@
-const ll N = 200007;
-ll fac[N],finv[N],inv[N];
-void cominit(ll mod){
-    fac[0]=fac[1]=1;
-    finv[0]=finv[1]=1;
-    inv[1]=1;
-    for(int i=2;i<N;i++){
-        fac[i]=fac[i-1]*i%mod;
-        inv[i]=mod-inv[mod%i]*(mod/i)%mod;
-        finv[i]=finv[i-1]*inv[i]%mod;
+struct Combination {
+    long long C_MOD;
+    vector<long long> fac, finv, inv;
+    Combination(long long n, long long mod) noexcept : C_MOD(mod) {
+        fac.resize(n, 0);
+        finv.resize(n, 0);
+        inv.resize(n, 0);
+        fac[0] = fac[1] = finv[0] = finv[1] = inv[1] = 1;
+        for(int i = 2; i < n; i ++) {
+            fac[i] = fac[i - 1] * i % mod;
+            inv[i] = mod - inv[mod % i] * (mod / i) % mod;
+            finv[i] = finv[i - 1] * inv[i] % mod;
+        }
     }
-}
-ll com(ll n,ll k){
-    if(n<k)return 0;
-    if(n<0 || k<0)return 0;
-    return fac[n]*(finv[k]*finv[n-k]%mod)%mod;
-}
-
-// cominit(MOD)が必要
-// 必要に応じてNを大きくする
+    long long com(long long n, long long k) {
+        if(n < k || n < 0 || k < 0) return 0;
+        return fac[n] * (finv[k] * finv[n - k] % C_MOD) % C_MOD;
+    }
+};
