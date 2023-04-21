94052150783f67bf677f1f5f7d4b5f623a6bbb40
shinchankosen <sophiaringoukosen@gmail.com>
2023-04-21 12:18:56 +0900

    nev

--- source/other/negative_vector.cpp
+++ source/other/negative_vector.cpp
@@ -1,11 +1,11 @@
 struct nev {
 private:
-    ll a;
-    vector<ll> v;
+    long long a;
+    vector<long long> v;
 public:
-    nev(ll a, ll n = 0):a(a), v(n, 0){}
+    nev(long long a, long long n = 0):a(a), v(n, 0){}
     void resize(int m) {v.resize(m);}
-    void push_back(ll n) {v.push_back(n);}
+    void push_back(long long n) {v.push_back(n);}
     size_t size() const {return v.size();}
-    ll& operator [] (int i) {return v[i - a];}
+    long long& operator [] (int i) {return v[i - a];}
 };
