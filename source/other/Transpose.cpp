63dfcb123e0e8e69dd7c72471365659056f832a7
shinchankosen <sophiaringoukosen@gmail.com>
2023-04-28 00:31:38 +0900

    transposeにstdつける

--- source/other/Transpose.cpp
+++ source/other/Transpose.cpp
@@ -1,11 +1,9 @@
-template<class T> vector<T> Transpose(const vector<T> &v) {
+template<class T> std::vector<T> Transpose(const std::vector<T> &v) {
     int n = v.size(), m = v[0].size();
-    vector<T> ret(m);
+    std::vector<T> ret(m);
     for(int i = 0; i < m; i ++) {
         ret[i].resize(n);
-        for(int j = 0; j < n; j ++) {
-            ret[i][j] = v[j][i];
-        }
+        for(int j = 0; j < n; j ++) ret[i][j] = v[j][i];
     }
     return ret;
 }
