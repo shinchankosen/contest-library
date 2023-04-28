232aa3c6eac6459631b3c727d847a716f72a3212
shinchankosen <sophiaringoukosen@gmail.com>
2023-04-28 10:28:34 +0900

    grid

--- source/other/Transpose.cpp
+++
@@ -1,9 +0,0 @@
-template<class T> std::vector<T> transpose(const std::vector<T> &v) {
-    int n = v.size(), m = v[0].size();
-    std::vector<T> ret(m);
-    for(int i = 0; i < m; i ++) {
-        ret[i].resize(n);
-        for(int j = 0; j < n; j ++) ret[i][j] = v[j][i];
-    }
-    return ret;
-}
