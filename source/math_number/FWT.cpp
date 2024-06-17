f54891dc6a23349580034c8fa7433cc8150653b8
shinchankosen <sophiaringoukosen@gmail.com>
2024-06-17 19:58:25 +0900

    xor conv

---
+++ source/math_number/FWT.cpp
@@ -0,0 +1,24 @@
+template <typename T>
+void fwt(vector<T>& f) {
+    int n = f.size();
+    for (int i = 1; i < n; i <<= 1) {
+        for (int j = 0; j < n; j++) {
+            if ((j & i) == 0) {
+                T x = f[j], y = f[j | i];
+                f[j] = x + y, f[j | i] = x - y;
+            }
+        }
+    }
+}
+template <typename T>
+void ifwt(vector<T>& f) {
+    int n = f.size();
+    for (int i = 1; i < n; i <<= 1) {
+        for (int j = 0; j < n; j++) {
+            if ((j & i) == 0) {
+                T x = f[j], y = f[j | i];
+                f[j] = (x + y) / 2, f[j | i] = (x - y) / 2;
+            }
+        }
+    }
+}
