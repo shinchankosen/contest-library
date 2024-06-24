fb0751e620b59ebfef3f942d272fca1246a7adcd
shinchankosen <sophiaringoukosen@gmail.com>
2024-06-24 19:52:40 +0900

    lca

--- source/graph/doubling.cpp
+++ source/graph/doubling.cpp
@@ -1,12 +1,13 @@
 struct Doubling {
 private:
-    std::vector<std::vector<int>> nx;
-    std::vector<long long> dis;
+    static constexpr int max_bit = 20;
+    std::vector<std::array<int, max_bit>> nx;
+    std::vector<int> dis;
 public:
     Doubling(std::vector<std::vector<int>> v, int s = 0) noexcept { 
         int n = v.size();
-        dis.resize(n, INF);
-        nx.resize(n, std::vector<int> (63, -1));
+        dis.resize(n, 1 << 30);
+        nx.resize(n);
         dis[s] = 0;
         auto dfs = [&] (auto dfs, int now, int p = -1) -> void {
             nx[now][0] = p;
@@ -17,36 +18,44 @@ public:
             }
         };
         dfs(dfs, s);
-        for(int j = 0; j < 62; j ++) for(int i = 0; i < n; i ++) {
+        for(int j = 0; j < max_bit - 1; j ++) for(int i = 0; i < n; i ++) {
             if(nx[i][j] != -1) nx[i][j + 1] = nx[nx[i][j]][j];
         }
     }
     Doubling(std::vector<int> nx_) noexcept {
         int n = nx_.size();
-        nx.resize(n, std::vector<int> (63, -1));
+        nx.resize(n);
         for(int i = 0; i < n; i ++) nx[i][0] = nx_[i];
-        for(int j = 0; j < 62; j ++) for(int i = 0; i < n; i ++) {
+        for(int j = 0; j < max_bit - 1; j ++) for(int i = 0; i < n; i ++) {
             if(nx[i][j] != -1) nx[i][j + 1] = nx[nx[i][j]][j];
         }
     }
+
     // nowのm個先 (LCAではm個親)
     int par(int now, long long m) {
-        for(long long i = 0, j = 1; j <= m; i ++, j <<= 1) {
+        while(m) {
             if(now == -1) return -1;
-            if(m & j) now = nx[now][i];
+            int t = __builtin_ctz(m);
+            now = nx[now][t];
+            m ^= 1LL << t;
         }
         return now;
     }
-    long long lca(long long a, long long b){
-        if(dis[a] < dis[b]) b = par(b, dis[b] - dis[a]);
-        else if(dis[a] > dis[b]) a = par(a, dis[a] - dis[b]);
-        long long le = -1, ri = dis[a];
-        while(ri - le > 1){
-            long long mid = (ri + le) / 2;
-            if(par(a, mid) == par(b, mid)) ri = mid;
-            else le = mid;
+
+    // Lowest Common Ancestor
+    int lca(int a, int b) {
+        if(dis[a] != dis[b]) {
+            if(dis[a] > dis[b]) swap(a, b);
+            b = par(b, dis[b] - dis[a]);
         }
-        return par(a, ri);
+        if(a == b) return a;
+        for(int i = __lg(dis[a]); i >= 0; i --) {
+            if(dis[a] < (1 << i)) continue;
+            if(nx[a][i] != nx[b][i]) a = nx[a][i], b = nx[b][i];
+        }
+        return nx[a][0];
     }
-    long long dist(long long a, long long b){return dis[a] + dis[b] - dis[lca(a, b)] * 2;}
+
+    // distance between a and b
+    int dist(int a, int b){return dis[a] + dis[b] - (dis[lca(a, b)] << 1);}
 };
