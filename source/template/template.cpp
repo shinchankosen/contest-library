87951b1709805b742af85b31ff705b2e80ec726e
shinchankosen <sophiaringoukosen@gmail.com>
2025-08-19 20:11:13 +0900

    template

---
+++ source/template/template.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v) (v).begin(),(v).end()
+#define pb emplace_back
+#define rep(i, n) for(int i=0;i<(n);i++)
+#define foa(e, v) for(auto& e : v)
+#define dout(a) cout<<fixed<<setprecision(10)<<a<<'\n';
+#define Cout(a) cout<<a<<'\n';
+
+using ll = long long;
+using ld = long double;
+using Int = __int128;
+template <class T> using pqr = priority_queue<T, vector<T>, greater<T>>;
+
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) {
+    bool compare = a < b;
+    if(compare) a = b;
+    return compare;
+}
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) {
+    bool compare = a > b;
+    if(compare) a = b;
+    return compare;
+}
+template <typename T> inline T back(std::set<T> &s) {
+    return *s.rbegin();
+}
+template <typename T> inline T back(std::multiset<T> &s) {
+    return *s.rbegin();
+}
+template <typename T> inline T pop_back(std::set<T> &s) {
+    auto it = prev(s.end());
+    T val = *it;
+    s.erase(it); 
+    return val;
+}
+template <typename T> inline T pop_back(std::multiset<T> &s) {
+    auto it = prev(s.end());
+    T val = *it;
+    s.erase(it); 
+    return val;
+}
+
+const int dy[8] = {-1, 0, 0, 1, 1, -1, 1, -1};
+const int dx[8] = {0, -1, 1, 0, -1, -1, 1, 1};
+
+const ll MOD7 = 1000000007, MOD998 = 998244353, INF = (3LL << 59);
+const int inf = 1 << 30;
+const char br = '\n';
+
+void solve() {
+    //
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int testcase = 1; 
+    // cin >> testcase;
+    while(testcase --) solve();
+
+    return 0;
+}
