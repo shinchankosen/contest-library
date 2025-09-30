9cffdb8f1cd70be3612f79f70f6173ba82c4ed5b
shinchankosen <sophiaringoukosen@gmail.com>
2025-10-01 02:37:37 +0900

    seg

---
+++ source/binary_tree/segtree.cpp
@@ -0,0 +1,86 @@
+template<class S, S (*op)(S, S), S (*e)()> struct segtree {
+    public:
+    explicit segtree(int n) : segtree(vector<S>(n, e())) {}
+    explicit segtree(const vector<S>& v) : _n(int(v.size())) {
+        size = 1;
+        while(size < _n) size *= 2;
+        log = __builtin_ctz(size);
+        d = vector<S>(2 * size, e());
+        for(int i = 0; i < _n; i++) d[size + i] = v[i];
+        for(int i = size - 1; i >= 1; i--) { update(i); }
+    }
+
+    void set(int p, S x) {
+        p += size;
+        d[p] = x;
+        for(int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const { return d[p + size]; }
+
+    S prod(int l, int r) const {
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while(l < r) {
+            if(l & 1) sml = op(sml, d[l++]);
+            if(r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+   S all_prod() const { return d[1]; }
+
+    template<class F> int max_right(int l, F f) {
+        if(l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while(l % 2 == 0) l >>= 1;
+            if(!f(op(sm, d[l]))) {
+                while(l < size) {
+                    l = (2 * l);
+                    if(f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while((l & -l) != l);
+        return _n;
+    }
+
+    template<class F> int min_left(int r, F f) {
+        if(r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while(r > 1 && (r % 2)) r >>= 1;
+            if(!f(op(d[r], sm))) {
+                while(r < size) {
+                    r = (2 * r + 1);
+                    if(f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while((r & -r) != r);
+        return 0;
+    }  
+
+    private:
+    int _n, size, log;
+    vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
